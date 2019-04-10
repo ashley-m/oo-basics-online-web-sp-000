7da17f80eb55656620a13e754e36879df2e2cbb3
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 00:18:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ attr_accessor :brand, :color, :size, :material, :condition
     @brand = brand
   end
   def cobble
-    puts "The shoe has been repaired."
+    puts "Your shoe is as good as new!"
     @condition = "new"
   end
 end
